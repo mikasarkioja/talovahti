207eb0410f70319131f7855ac223a4d6e441e580
Mika Särkioja <mika.sarkioja@isannat.fi>
2026-01-13 16:14:23 +0200

    fix: Revert PrismaClient initialization back to datasources for compatibility with build

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,7 +2,11 @@ import { PrismaClient } from '@prisma/client'
 import { hash } from 'bcrypt'
 
 const prisma = new PrismaClient({
-  datasourceUrl: process.env.DIRECT_URL || process.env.DATABASE_URL
+  datasources: {
+    db: {
+      url: process.env.DIRECT_URL || process.env.DATABASE_URL
+    }
+  }
 })
 
 async function main() {
